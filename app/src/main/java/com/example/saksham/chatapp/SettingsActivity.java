2130f2b5b38285c324f6feb90ddb1fc02c59dbe7
saksham1211 <35836123+saksham1211@users.noreply.github.com>
2018-09-05 22:44:12 +0530

    Minor Changes and updates.
    Signed-off-by: saksham1211 <35836123+saksham1211@users.noreply.github.com>

--- app/src/main/java/com/example/saksham/chatapp/SettingsActivity.java
+++ app/src/main/java/com/example/saksham/chatapp/SettingsActivity.java
@@ -304,17 +304,4 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-    public static String random() {
-        Random generator = new Random();
-        StringBuilder randomStringBuilder = new StringBuilder();
-        int randomLength = generator.nextInt(20);
-        char tempChar;
-        for (int i = 0; i < randomLength; i++){
-            tempChar = (char) (generator.nextInt(96) + 32);
-            randomStringBuilder.append(tempChar);
-        }
-        return randomStringBuilder.toString();
-    }
-
-
 }
